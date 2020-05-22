e05f2213fe7fb882f8d82ea67c1fb9840c386271
astrouga <64537657+astrouga@users.noreply.github.com>
2020-05-22 08:19:10 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC51/e9L04RLoYJ/6rTTXhrgkb0d7qXE6g320bqxD/ZDGcKDTzYRZc8/DJN8MS5Mfq6p6MRKQ2JqO/D2D9UjQ0DIrnxZeaLus317WCS9vpPusWckzHcwZHl/jkS6+hWR6+9xokrf0+Meg8zB7sS4bEeRJbqD5+kNtGVbdVMaJQdGRsy3r6pxErnL64YHmxxYZwSs3suve+sIgsXNYBGygaH/ygluYEiQbZlGfE9gKfDN36PYy4ii071YbYTjyBS3Ej5r3MKKpBWBQXf0amwXk+/jx//cSfIM8sTwM1sc/tHXYcjx0yEwns5s73gaORen8L2MCG34FcWuIcg9MoAXXI5',
 	}  
 }
